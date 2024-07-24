de5ba354a7c5397838886b543b858d14bee2a03d
Renzo <renzopayero@hotmail.com>
2024-07-24 19:02:57 -0300

    adelantando tablas y ruteo

---
+++ database/seeders/CategoriaSeeder.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+
+class CategoriaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('categorias')->insert([
+            'nombre' => 'Indumentaria',
+            'icono' => 'indumentaria',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        
+        DB::table('categorias')->insert([
+            'nombre' => 'Balones',
+            'icono' => 'balon',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        
+        DB::table('categorias')->insert([
+            'nombre' => 'Botines',
+            'icono' => 'guantes',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        
+        DB::table('categorias')->insert([
+            'nombre' => 'Guantes',
+            'icono' => 'guantes',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        
+        DB::table('categorias')->insert([
+            'nombre' => 'Canilleras',
+            'icono' => 'canilleras',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        
+        DB::table('categorias')->insert([
+            'nombre' => 'Accesorios',
+            'icono' => 'accesorios',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,11 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoriaController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
 Route::get('/categorias', [CategoriaController::class, 'index'] );
