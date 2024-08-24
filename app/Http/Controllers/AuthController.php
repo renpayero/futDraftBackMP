669f1f3d41c3a57d1278c6b6d180cc70fa39b3e6
Renzo <renzopayero@hotmail.com>
2024-08-24 15:25:16 -0300

    modulo de seguridad completo

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,9 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\RegistroRequest;
+use App\Http\Requests\LoginRequest;
 use Illuminate\Http\Request;
 use App\Models\User;
-
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -22,10 +23,27 @@ class AuthController extends Controller
             'user' => $user
         ];
     }
-    public function login(Request $request){
-
+    public function login(LoginRequest $request){
+        $data = $request->validated();
+        
+        //revisar el password
+        if(!Auth::attempt($data)){
+            return response([
+                'errors' => ['El email o la contraseña son incorrectos']
+            ], 422);
+        }
+        // autenticar usuario
+        $user = Auth::user();
+        return [
+            'token' => $user->createToken('token')->plainTextToken,
+            'user' => $user
+        ];
     }
     public function logout(Request $request){
-
+        $user = $request->user();
+        $user->currentAccessToken()->delete();
+        return response([
+            'user' => 'Sesión cerrada'
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,12 +6,16 @@ use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\ProductoController;
 use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function() {
+    Route::get('/user', function(Request $request){
+        return $request->user();
+    });
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
 
 Route::apiResource('/categorias', CategoriaController::class );
 Route::apiResource('/productos', ProductoController::class );
 
 //Authenticacion
 Route::post('/registro', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
